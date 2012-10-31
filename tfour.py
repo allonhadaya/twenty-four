dbaffcd8a4a12ce80170c85c14ad2bf0884c6c22
Allon Hadaya <allonhadaya@hotmail.com>
2012-10-31 02:17:09 -0400

    Generalize the solution for targets other than 24, a different number of operands, and a different number of operators.

--- tfour.py
+++ tfour.py
@@ -1,22 +1,47 @@
 from sys import argv
-from itertools import product, permutations
+from itertools import product, permutations, combinations, repeat
 
 def main():
-  for s in solutions():
-    print present(s)
+  operands = [float(x) for x in argv[1:]]
+  operators = [add, sub, mul, div]
+  symbols = { add: '+', sub: '-', mul: '*', div: '/' }
 
-def solutions():  
-  options = [calc(a, b) for a in permutations(operands) for b in product(operators, repeat=3)]
-  return [opt for opt in options if opt[2] == 24.0]
+  for s in solutions(operators, operands, 24):
+    print present(s, symbols)
 
-def calc(a, b):
+  deckOfCards = range(1, 14) + range(1, 14) + range(1, 14) + range(1, 14)
+  allOperands = combinations(deckOfCards, 4)
+
+#  numerator = devisor = 0
+#  for operands in allOperands:
+#    devisor = devisor + 1
+#    if len(solutions(operators, operands, 24)) > 0:
+#      numerator = numerator + 1
+
+#  print 'all:%d' % devisor
+#  print 'solutions:%d' % numerator
+#  print 'probability of having a solution:%f' % (float(numerator) / devisor)
+
+def solutions(operators, operands, target):
+  numberOfOperators = len(operands) - 1
+  options = [evaluate(a, b) for a in permutations(operands) for b in product(operators, repeat=numberOfOperators)]
+  return [o for o in options if o[2] == target]
+
+def evaluate(operands, operators):
   try:
-    return (a, b, b[2](b[1](b[0](a[0], a[1]), a[2]), a[3]))
-  except ZeroDivisionError:
-    return (a, b, float('NaN'))
+    result = operands[0]
+    for i in range(len(operators)):
+      result = operators[i](result, operands[i+1])
+  except:
+    result = None
+  return (operands, operators, result)
 
-def present(opt):  
-  return '%d = (((%d %s %d) %s %d) %s %d)' % (opt[2], opt[0][0], sym[opt[1][0]], opt[0][1], sym[opt[1][1]], opt[0][2], sym[opt[1][2]], opt[0][3])
+def present(solution, symbols):
+  numOps = len(solution[1])
+  value = '(' * numOps + str(solution[0][0])
+  for i in range(numOps):
+    value = value + ' ' + symbols[solution[1][i]] + ' ' + str(solution[0][i + 1]) + ')'
+  return value
 
 def add(a, b):
   return a + b
@@ -29,9 +54,5 @@ def mul(a, b):
 
 def div(a, b):
   return a / b
-
-operands = [float(x) for x in argv[1:5]]
-operators = [add, sub, mul, div]
-sym = { add: '+', sub: '-', mul: '*', div: '/' }
 
 main()
