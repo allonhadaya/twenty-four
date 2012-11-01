f95760f6ee85289c5a2234c38f8e66a4588ce382
Allon Hadaya <allonhadaya@hotmail.com>
2012-11-01 13:27:45 -0400

    Grab some statistics.

--- tfour.py
+++ tfour.py
@@ -1,26 +1,31 @@
 from sys import argv
-from itertools import product, permutations, combinations, repeat
+from itertools import product, permutations, repeat
 
 def main():
-  operands = [float(x) for x in argv[1:]]
+# operands = [float(x) for x in argv[1:]]
+
   operators = [add, sub, mul, div]
   symbols = { add: '+', sub: '-', mul: '*', div: '/' }
 
-  for s in solutions(operators, operands, 24):
-    print present(s, symbols)
+# for s in solutions(operators, operands, 24):
+#   print present(s, symbols)
 
-  deckOfCards = range(1, 14) + range(1, 14) + range(1, 14) + range(1, 14)
-  allOperands = combinations(deckOfCards, 4)
-
-#  numerator = devisor = 0
-#  for operands in allOperands:
-#    devisor = devisor + 1
-#    if len(solutions(operators, operands, 24)) > 0:
-#      numerator = numerator + 1
-
-#  print 'all:%d' % devisor
-#  print 'solutions:%d' % numerator
-#  print 'probability of having a solution:%f' % (float(numerator) / devisor)
+  a = b = c = 0
+  for operands in allOps():
+    s = len(solutions(operators, operands, 24))
+    b = b + 1
+    if s > 0:
+      a = a + 1
+      c = c + s
+    
+  print '''
+a = %d
+b = %d
+c = %d
+a / b = %d / %d = %f
+c / a = %d / %d = %f
+c / b = %d / %d = %f
+''' % (a, b, c, a, b, float(a) / b, c, a, float(c) / a, c, b, float(c) / b)
 
 def solutions(operators, operands, target):
   numberOfOperators = len(operands) - 1
@@ -42,6 +47,13 @@ def present(solution, symbols):
   for i in range(numOps):
     value = value + ' ' + symbols[solution[1][i]] + ' ' + str(solution[0][i + 1]) + ')'
   return value
+
+def allOps():
+  for a in range(1, 14):
+    for b in range(1, 14):
+      for c in range(1, 14):
+        for d in range(1, 14):
+          yield [a, b, c, d]
 
 def add(a, b):
   return a + b
